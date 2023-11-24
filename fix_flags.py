eade4fd5f062e854a55dce6c72f560888106f711
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-11-24 23:51:35 +0200

    changes the flags, to flagsapi

---
+++ fix_flags.py
@@ -0,0 +1,20 @@
+import json
+
+file_path = "./db.json"
+flag_type = 'flat' # could also be 'shiny'
+size = 64 # flag size pixels aka 64px by 64px
+
+with open(file_path, 'r') as file:
+    data_base = json.load(file)
+
+for data in data_base['countries']:
+
+    link_end = f'{data["code"]}/{flag_type}/{size}.png'
+    data['flag'] = f'https://flagsapi.com/{link_end}'
+
+    print("Succsessfully Added:", data['name'])
+
+data = json.dumps(data_base, indent=2)
+
+with open(file_path, 'w') as updated_file:
+    updated_file.write(data)
